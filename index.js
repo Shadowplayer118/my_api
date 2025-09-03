3fff3cdec0726b4f5f3d39904fbbb43d58c23a1b
Shadowplayer118 <dhanielmalinao@gmail.com>
2025-09-03 15:07:58 +0800

    pool

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ app.use(cors());
 app.use(express.json());
 
 // ✅ MySQL connection
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
+
 
 db.connect((err) => {
   if (err) {
