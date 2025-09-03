25788aab31b58a5a599ad0bbf2604ca80d08c2c6
Shadowplayer118 <dhanielmalinao@gmail.com>
2025-09-03 14:20:53 +0800

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from my API!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
